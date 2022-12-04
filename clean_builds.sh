ffa4d89cd3e1ecd83de120f6c1294460a30e9ef8
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-04 18:01:46 +0000

    updates to gitignore and clean_builds.sh

--- clean_builds.sh
+++ clean_builds.sh
@@ -6,10 +6,12 @@ find . -name "*.out" -print -delete 2>/dev/null
 find . -name "*.x" -print -delete 2>/dev/null
 find . -name "cmake_install.cmake" -print -delete 2>/dev/null
 find . -name "CMakeCache.txt" -print -delete 2>/dev/null
+find . -name "CTestTestfile.cmake" -print -delete 2>/dev/null
+find . -name "CPackConfig.cmake" -print -delete 2>/dev/null
 find . -iname "MakeFile" -print -delete 2>/dev/null
 find . -type d -name ".ccls-cache" -print -exec rm -rf "{}" \; 2>/dev/null
 find . -type d -name ".cmake" -print -exec rm -rf "{}" \; 2>/dev/null
-find . -type d -name "build" -print -exec rm -rf "{}" \; 2>/dev/null
+find . -type d -name "build_*" -print -exec rm -rf "{}" \; 2>/dev/null
 find . -type d -name "buildtree" -print -exec rm -rf "{}" \; 2>/dev/null
 find . -type d -name "CMakeFiles" -print -exec rm -rf "{}" \; 2>/dev/null
 find . -type d \( -name "debug" -o -name "release" \) -print -exec rm -rf "{}" \; 2>/dev/null
