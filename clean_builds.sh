af254cab2a115ab66ddeb780c92a1899f78b1dd2
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-04 16:14:00 +0000

    updates the clean_builds.sh file and moves to root of build tree

--- clean_builds.sh
+++ clean_builds.sh
@@ -6,6 +6,7 @@ find . -name "*.out" -print -delete 2>/dev/null
 find . -name "*.x" -print -delete 2>/dev/null
 find . -type d \( -name "debug" -o -name "release" \) -print -exec rm -rf "{}" \; 2>/dev/null
 find . -type d -name "build" -print -exec rm -rf "{}" \; 2>/dev/null
+find . -type d -name "buildtree" -print -exec rm -rf "{}" \; 2>/dev/null
 find . -type d -name ".ccls-cache" -print -exec rm -rf "{}" \; 2>/dev/null
 
 echo "Removed build-output files. DONE!"
